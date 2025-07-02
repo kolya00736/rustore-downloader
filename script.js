dfac29f6a49e5dd488b5c2c416e4fd21979f703b
KotovNikita16 <kotoff123@gmail.com>
2025-07-02 13:44:45 +0700

    Data field "createdAt" changed to "firstPublishedAt"

--- script.js
+++ script.js
@@ -202,7 +202,7 @@ function createAppCard(appDetails, app) {
                 <div>Version: ${appDetails.versionName}</div>
                 <div>Downloads: ${appDetails.downloads.toLocaleString()}</div>
                 <div>Updated: ${formatDate(appDetails.appVerUpdatedAt)}</div>
-                <div>Added: ${formatDate(appDetails.createdAt)}</div>
+                <div>Added: ${formatDate(appDetails.firstPublishedAt)}</div>
             </div>
             
             <div class="mt-4 flex justify-between items-center">
